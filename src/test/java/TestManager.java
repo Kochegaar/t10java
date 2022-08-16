87bce79bda15f5a7ebfff686248b6d4e46cb04f0
kochegaar <106970574+Kochegaar@users.noreply.github.com>
2022-08-16 11:03:21 +0300

    intelij10

---
+++ src/test/java/TestManager.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestManager {
+
+     @Test
+     public void test1() {
+
+          String movie1 = "movie1";
+          String movie2 = "movie2";
+          String movie3 = "movie3";
+          String movie4 = "movie4";
+          String movie5 = "movie5";
+
+          Manager manager = new Manager();
+
+          manager.add(movie1);
+          manager.add(movie2);
+          manager.add(movie3);
+          manager.add(movie4);
+          manager.add(movie5);
+
+          String[] expected = {
+                  movie1,
+                  movie2,
+                  movie3,
+                  movie4,
+                  movie5,
+
+          };
+          String[] actual = manager.findAll();
+          Assertions.assertArrayEquals(expected, actual);
+
+     }
+}
